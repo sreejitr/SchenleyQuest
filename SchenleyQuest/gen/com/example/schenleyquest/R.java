138a1827f210ed1e043a7d888b97da3d7f5f7716
Abhishek Mukhopadhyay <amukhop1@server.fake>
2014-02-27 02:32:12 -0500

    Question screen with options

--- SchenleyQuest/gen/com/example/schenleyquest/R.java
+++ SchenleyQuest/gen/com/example/schenleyquest/R.java
@@ -24,14 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f08000c;
         public static final int button1=0x7f080001;
         public static final int button2=0x7f080002;
         public static final int button3=0x7f080003;
         public static final int button4=0x7f080000;
-        public static final int hint_button=0x7f080005;
-        public static final int submit_button=0x7f080006;
-        public static final int text_view_question=0x7f080004;
+        public static final int hint_button=0x7f080004;
+        public static final int radioButton1=0x7f080007;
+        public static final int radioButton2=0x7f080008;
+        public static final int radioButton3=0x7f080009;
+        public static final int radioButton4=0x7f08000a;
+        public static final int radioOptions=0x7f080006;
+        public static final int submit_button=0x7f08000b;
+        public static final int text_view_question=0x7f080005;
     }
     public static final class layout {
         public static final int activity_hints=0x7f030000;
@@ -48,6 +53,10 @@ public final class R {
         public static final int transition_screen=0x7f070004;
     }
     public static final class string {
+        public static final int Option1=0x7f05000d;
+        public static final int Option2=0x7f05000e;
+        public static final int Option3=0x7f05000f;
+        public static final int Option4=0x7f050010;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
--- SchenleyQuest/src/com/example/schenleyquest/Questions.java
+++ SchenleyQuest/src/com/example/schenleyquest/Questions.java
@@ -2,18 +2,30 @@ package com.example.schenleyquest;
 
 import java.io.IOException;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Questions extends Activity {
-
+	RadioGroup radioOpGroup;
+	RadioButton radioOp;
+	TextView questionText;
+	String selectedOption;
+	String correctOption;
+	String featureId = "";
+	String qId ="";
+	String question = "";
+	String[] option = new String[4];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,8 +48,9 @@ public class Questions extends Activity {
 	 
 	 	}
 		dbHelper.close();
-		String question = "";
+		
 		String[] inputParameters = getIntent().getStringExtra(Main.KEY_QUESTION).split("\\s+");
+		featureId = inputParameters[0];
 		if (inputParameters.length == 1) {
 			SQLiteDatabase db = dbHelper.getReadableDatabase();
 
@@ -50,7 +63,7 @@ public class Questions extends Activity {
 			
 			String selection = Contract.Questions.COLUMN_NAME_FEATURES_ID + "=?";
 			
-			String[] selectionArgs = {inputParameters[0]};
+			String[] selectionArgs = {featureId};
 
 			// How you want the results sorted in the resulting Cursor
 			String sortOrder =
@@ -67,14 +80,51 @@ public class Questions extends Activity {
 			    );
 			
 			cursor.moveToFirst();
+			
+			qId = Integer.toString(cursor.getInt(
+				    cursor.getColumnIndexOrThrow(Contract.Questions._ID)));
 			question = cursor.getString(
 			    cursor.getColumnIndexOrThrow(Contract.Questions.COLUMN_NAME_QUESTION)
 			);
+			
+			String[] optionProjection = {
+					Contract.Options._ID,
+				    Contract.Options.COLUMN_NAME_OPTION,
+				    Contract.Options.COLUMN_NAME_FEATURES_ID
+				    };
+			selection = Contract.Options.COLUMN_NAME_QUESTION_ID + "=?";
+			selectionArgs[0] = qId;
+			sortOrder = Contract.Options._ID + " ASC";
+			
+			cursor = db.query(
+					Contract.Options.TABLE_NAME,  // The table to query
+				optionProjection,                               // The columns to return
+			    selection,                                // The columns for the WHERE clause
+			    selectionArgs,                            // The values for the WHERE clause
+			    null,                                     // don't group the rows
+			    null,                                     // don't filter by row groups
+			    sortOrder                                 // The sort order
+			    );
+			cursor.moveToFirst();
+			for (int i = 0; i < 4; i ++){
+				option[i] = cursor.getString(
+					    cursor.getColumnIndexOrThrow(Contract.Options.COLUMN_NAME_OPTION));
+				if(Integer.toString(cursor.getInt(
+					    cursor.getColumnIndexOrThrow(Contract.Options.COLUMN_NAME_FEATURES_ID))).equals(featureId))
+					correctOption = Integer.toString(i);
+				cursor.moveToNext();
+			}
+			
 		}
 		
 		setContentView(R.layout.activity_questions);
-		TextView questionText = (TextView)findViewById(R.id.text_view_question);
+		questionText = (TextView)findViewById(R.id.text_view_question);
 		questionText.setText(question);
+		radioOpGroup = (RadioGroup) findViewById(R.id.radioOptions);
+		for (int i = 0; i < radioOpGroup .getChildCount(); i++) {
+            ((RadioButton) radioOpGroup.getChildAt(i)).setText(option[i]);
+        }
+		
 	}
 
 	@Override
@@ -84,18 +134,47 @@ public class Questions extends Activity {
 		return true;
 	}
 	
+	public void questionRadioButtonClick(View view) {
+		Button submitButton = (Button)findViewById(R.id.submit_button);
+		if (submitButton.getVisibility() == View.INVISIBLE)
+			submitButton.setVisibility(View.VISIBLE);
+			
+		switch(view.getId())
+		{
+		case R.id.radioButton1:
+			selectedOption = "1";
+			break;
+		case R.id.radioButton2:
+			selectedOption = "2";
+			break;
+		case R.id.radioButton3:
+			selectedOption = "3";
+			break;
+		case R.id.radioButton4:
+			selectedOption = "4";
+			break;
+		default:
+	    	throw new RuntimeException("Unknown button ID");
+		}
+	}
+	
+	
+	
     /** Called when the user clicks the Level 1, Level 2, Level 3 buttons or the Settings button */
     public void questionsButtonClick(View view) {
     	
     	switch(view.getId())
     	{
     	case R.id.submit_button:
-    		Intent intent1 = new Intent(this, TransitionScreen.class);
-        	startActivity(intent1);
+    		String correctAnswer = (selectedOption.equals(correctOption)) ? "yes" : "no";
+    		Intent intentSubmit = new Intent(this, TransitionScreen.class);
+    		intentSubmit.putExtra(Main.KEY_QUESTION, featureId + " " + qId + " " + correctAnswer + " " + Integer.toString((Integer.parseInt(featureId) + 1)));
+        	startActivity(intentSubmit);
+        	this.finish();
         	break;
     	case R.id.hint_button:
-    		Intent intent2 = new Intent(this, Hints.class);
-        	startActivity(intent2);
+    		Intent intentHint = new Intent(this, Hints.class);
+        	startActivity(intentHint);
         	break;
 
     	default:
